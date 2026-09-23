7a068e3686a780285ea3221fb41492abf98470e4
alanstokes <alan.stokes@stfc.ac.uk>
2026-09-23 13:54:01 +0100

    flake 8

--- starbug2/core/custom_psf_gui/custom_psf_gui.py
+++ starbug2/core/custom_psf_gui/custom_psf_gui.py
@@ -12,6 +12,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+
 from typing import Tuple
 
 from PyQt6.QtCore import Qt
